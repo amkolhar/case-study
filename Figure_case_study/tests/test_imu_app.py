56655130f0a7595cde7fdefebbbfd75e057c6534
Atharv Kolhar <47504988+amkolhar@users.noreply.github.com>
2024-10-03 13:09:39 -0700

    Update test_imu_app.py

--- Figure_case_study/tests/test_imu_app.py
+++ Figure_case_study/tests/test_imu_app.py
@@ -19,7 +19,8 @@ class TestIMUApp(BaseTest):
 
         resp_roll_angle = self.imu_comm.get_roll_angle(100)[-1]
 
-        assert resp_roll_angle-self.roll_err == approx(roll_angle_set, abs=threshold), f"When Roll angle set to {roll_angle_set}, IMU reported {resp_roll_angle} which is not within the 0.1 degree threshold"
+        assert resp_roll_angle-self.roll_err == approx(roll_angle_set, abs=threshold), 
+                        f"When Roll angle set to {roll_angle_set}, IMU reported {resp_roll_angle} which is not within the 0.1 degree threshold"
 
     # Testcase : 001.2
     @pytest.mark.parametrize("req_pitch_angle", [-40,-30,-20,-10,10, 20, 30, 40])
@@ -30,4 +31,5 @@ class TestIMUApp(BaseTest):
 
         resp_pitch_angle = self.imu_comm.get_pitch_angle(100)[-1]
 
-        assert resp_pitch_angle-self.pitch_err == approx(pitch_angle_set, abs=threshold), f"When Roll angle set to {pitch_angle_set}, IMU reported {resp_pitch_angle} which is not within the 0.1 degree threshold"
+        assert resp_pitch_angle-self.pitch_err == approx(pitch_angle_set, abs=threshold), 
+                        f"When Roll angle set to {pitch_angle_set}, IMU reported {resp_pitch_angle} which is not within the 0.1 degree threshold"
